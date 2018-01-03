9866b397bf753bff0394f40a169a76c35d015276
Jimmy Saul <jamesstephensaul@gmail.com>
2018-01-03 12:09:18 +0000

    added my most basic essential

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+
+brew install bash-completion
+
+brew install homebrew/completions/brew-cask-completion
+
+# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
+brew install hub
+
+# Tree. displays directory tree, in color
+brew install tree
+
+# Install other useful binaries
+brew install node # This installs `npm` too using the recommended installation method
+
+brew install zsh
+
+# Remove outdated versions from the cellar
+brew cleanup
